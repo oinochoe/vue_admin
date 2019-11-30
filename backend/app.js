40bf6898e38d7e91582a0b673e0f240a12bb943b
Yeongmin Kim <oinochoe@naver.com>
2019-12-01 04:28:45 +0900

    moca admin 진행중

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,49 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var bodyParser = require("body-parser");
+// movie router
 var moviesRouter = require("./routes/movies");
+// mysql
+var mysql = require("mysql");
+
+// Connection 객체 생성
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3308,
+  user: "root",
+  password: "admin",
+  database: "test"
+});
+
+// Connect
+connection.connect(function(err) {
+  if (err) {
+    console.error("mysql connection error");
+    console.error(err);
+    throw err;
+  }
+});
 
 var app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// insert
+app.post("/regist", function(req, res) {
+  var user = {
+    userid: req.body.userid,
+    name: req.body.name,
+    address: req.body.address
+  };
+  connection.query("insert into users set ?", user, function(err, result) {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    res.status(200).send("success");
+  });
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
