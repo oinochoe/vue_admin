4b4063ca8c8124038e71fd961d6246d6572ceead
Yeongmin Kim <oinochoe@naver.com>
2019-12-11 22:01:39 +0900

    login 개선

--- backend/router/login.js
+++ backend/router/login.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // 로그인 페이지를 위한 코드
 router.get("/", function(req, res, next) {
   if (!req.session.name) {
-    res.redirect("/");
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
   } else {
     res.redirect("/list");
   }
